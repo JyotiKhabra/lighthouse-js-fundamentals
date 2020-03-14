fc9340db93fb8109365e6963b6cab80f11c21bd7
vagrant <j_khabra11@hotmail.com>
2020-03-14 06:47:02 +0000

    Odd numbers

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function (num) {
+  if (num % 2 === 0) {
+    return "true"
+  } else {
+    return "false"
+  }
+}
+console.log("6 is odd:" + isOdd(6));
+console.log("5 is odd:" + isOdd(5));
